dcb9ec975f13e8d309ddffe873fbbe806b4dfb51
show <szymomac@gmail.com>
2018-05-30 21:13:38 +0000

    poprawka

--- blog/settings.py
+++ blog/settings.py
@@ -121,4 +121,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-LOGIN_REDIRECT_URL = '/'
+
+
+
+#LOGIN_REDIRECT_URL = '/'
+
+MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+>>>>>>> Stashed changes
