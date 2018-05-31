73fed8b8d0157963c46d2cde1185c6c22d27b11e
show <szymomac@gmail.com>
2018-05-31 11:51:01 +0200

    dodawanie postow commit

--- blog_app/forms.py
+++ blog_app/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.models import *
+from .models import *
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput)
@@ -7,3 +8,10 @@ class UserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'first_name', 'last_name', 'email', 'password']
+
+
+class ArticleForm(forms.ModelForm):
+
+    class Meta:
+        model = Artykul
+        fields = ['nazwa', 'tresc']
